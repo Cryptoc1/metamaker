a5dc3be2988fb663fc6373864c194e8c6ccac9ae
Samuel Steele <steelesam72@gmail.com>
2015-04-14 20:18:19 -0400

    just did some stuff

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import imdb
+import os, sys, time
 
 api = imdb.IMDb()
 
-mov = api.search_movie('tt0470752')[0]
+print "Enter the IMDb id"
+mov = api.search_movie(raw_input("> "))[0]
 api.update(mov)
 
 # Prints info about Ex Machina
-print mov['runtime'][0], mov['rating'], mov['director'][0]
+print mov['plot']
