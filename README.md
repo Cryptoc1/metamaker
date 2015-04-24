23fc27936067420ab236aa45f433d083c322d13a
Samuel Steele <steelesam72@gmail.com>
2015-04-23 22:18:54 -0400

    Got it to work. 😎

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # MetaMaker
 MetaMaker is a simple pyhton utility I created to write metadata to movie file (mp4, m4v, mkv, avi, wmv).
 
-# How To Use
+## How To Use
 Using MetaMaker is quite simple really:
 
 		$ metamaker foo.mp4
 
 You will then be prompted for the IMdB URL for the movie and the program will write the metadata for you.
-### Plain and Simple
+
+## Dependencies
+* MP4Box
+
+MP4Box can be installed with brew
+''' brew install mp4box
+
+#### Plain and Simple
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
 
 import imdb
-import os, sys, time
+import os, sys
 
-api = imdb.IMDb()
+def main(id, dir):
+    api = imdb.IMDb()
 
-print "Enter the IMDb id"
-mov = api.search_movie(raw_input("> "))[0]
-api.update(mov)
+    if id == None:
+        print "Enter the IMDb id"
+        id = raw_input("> ")
+    else:
+        id = id
+    if dir == None:
+        print "Enter the filename (or path to)"
+        dir = os.path.abspath(raw_input("> "))
+    else:
+        dir = os.path.abspath(dir)
+        dir = dir.replace(" ", "\\ ")
 
-# Prints info about Ex Machina
-print mov['plot']
+    mov = api.search_movie(id)[0]
+    api.update(mov)
+
+    print "Downloading cover to ~/temp..."
+    cov_url = mov['cover']
+    os.system("wget -O ~/temp/" + id + ".jpg " + cov_url)
+    cov_path = os.path.expanduser("~/temp/" + id + ".jpg")
+   
+    print "Writing data to file..."
+    os.system("MP4Box -itags cover=\"" + cov_path + "\":tool=\"metamaker\":genre=\"" + mov['genre'][0] + "\":writer=\"" + mov['writer'][0]['name'] + "\":name=\"" + mov['title'] + "\":created=\"" + str(mov['year']) + "\":encoder=\"MP4Box\" " + dir + "")
+
+    print "Process complete, exiting."
+    sys.exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        main(None, None)
